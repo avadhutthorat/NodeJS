9be6e1278dc93f40337e2eabb75111efe1268e6e
Avadhut Thorat <avadhutthorat40@gmail.com>
2019-12-26 19:17:09 +0530

    Created Dummy user in sequelize

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const rootDir = require("./utils/path");
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 const errorController = require("./controllers/error");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -14,17 +16,42 @@ app.set("view engine", "pug"); // configuring pug for express
 app.set("views", "views"); // will look for view in views folder
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
 
 app.use(errorController.get404Page);
 
+Product.belongsTo(User, { constraints: true, onDelete: "cascade" });
+User.hasMany(Product);
+
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+// sync({ force: true }) - use this when you want to overwrite the tables
 sequelize
   .sync()
-  .then(() => {
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Avadhut", email: "thorat@test.com" });
+    }
+    return user;
+  })
+  .then(user => {
     app.listen(3000);
   })
-  .catch(err => console.log("Unable start the connection with database"));
+  .catch(err => {
+    console.log(err);
+  });
